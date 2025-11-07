0b8c2b9f2ffe962d78583024f2ccb0f031c0be35
sinha001 <sinhasonu004@gmail.com>
2025-11-07 16:18:23 +0530

    Created pattern.java By First Person

---
+++ pattern.java
@@ -0,0 +1,14 @@
+public class pattern{
+    public static void main(String args[])
+    {
+        int noOfExec = 5;
+        for(int i = 0;i<noOfExec;i++)
+        {
+            for(int j= 0;j<noOfExec;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
