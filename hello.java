ebdac8da8c158c136ecba706085e37296408d448
G S Sai Teja <sai1881tejags@gmail.com>
2025-11-07 16:18:12 +0530

    Updated hello.java by second person

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 public class hello {
     public static void main(String args[])
     {
-        System.out.println("Hello World!!");
+	for(int i=1;i<=10;i++) {
+	        System.out.println(i + "Hello World!!");
+	}
     }
 }
