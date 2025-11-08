2283ea30cf03444fd1496d54cabff7b3d23c2fea
saravanapandian170 <saravanapandian00@gmail.com>
2025-11-08 16:26:38 +0530

    Changes in hello.java from third person for Stretch task-3

--- hello.java
+++ hello.java
@@ -5,8 +5,8 @@ public class hello {
 	        System.out.println(i + "Hello World!!");
 	}
     //Print 1 to 10
-    for(int i = 1; i <= 10; i++){
-        System.out.println(i);
+    for(int i = 0; i <  10; i++){
+        System.out.println(i+1);
     }
     }
 }
